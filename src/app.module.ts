aced7c3ab7ce30d2837f279473591cb41eb3607b
MilitsaTsvetkova <militsa88@yahoo.com>
2025-02-24 10:15:45 +0200

    initial setup setting up absolute path

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { TagModule } from '@app/tag/tag.module';
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [TagModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
